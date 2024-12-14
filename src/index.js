bd7498051f483c80025ad9d95e2ca824bfaefcf9
Raihan Sharif <raihansharif92@gmail.com>
2024-12-14 21:16:49 +0000

    Better drop down

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import "./styles.css";
-import { dropdownEventHandler } from "./dropdown.js";
 
-const plantsBtn = document.getElementById("btn-plants");
-const toolsBtn = document.getElementById("btn-tools");
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
 
-plantsBtn.addEventListener("click", (event) => {
-  console.log("clicking plantsBtn");
-  dropdownEventHandler(event.target);
+const toggleDropDown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+dropdownBtn.addEventListener("click", function (e) {
+  // stops the toggleDropDown function being passed to
+  // the button's parent element, thus triggering it twice
+  e.stopPropagation();
+  toggleDropDown();
 });
 
-toolsBtn.addEventListener("click", (event) => {
-  console.log("clicking tools button");
-  dropdownEventHandler(event.target);
+// if the document itself, i.e. anything other than the drop down button
+// is clicked, the drop down menu is hidden
+// this is why stopPropagation is needed.
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropDown();
+  }
 });
